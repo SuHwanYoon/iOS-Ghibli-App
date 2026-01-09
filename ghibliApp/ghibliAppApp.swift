40a3f281e81be211db1be3317ce4b7be3d8c8c77
SuHwanYoon <suhwan6@gmail.com>
2026-01-09 16:12:37 +0900

    Initial Commit

---
+++ ghibliApp/ghibliAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ghibliAppApp.swift
+//  ghibliApp
+//
+//  Created by YOON on 1/9/26.
+//
+
+import SwiftUI
+
+@main
+struct ghibliAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ghibliAppTests/ghibliAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ghibliAppTests.swift
+//  ghibliAppTests
+//
+//  Created by YOON on 1/9/26.
+//
+
+import Testing
+@testable import ghibliApp
+
+struct ghibliAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
