dd940206b60de03af7123d2e25941a2596fd805b
SuHwanYoon <suhwan6@gmail.com>
2026-01-24 16:02:35 +0900

    Person Model에 상태구분을 위해 Equtable FilmDetailViewModel에 상태를 추가하고 로직함수에서 [Person]형태의 데이터와 Person형태의 데이터를 가져옴

--- ghibliApp/Networking/FilmDetailViewModel.swift
+++ ghibliApp/Networking/FilmDetailViewModel.swift
@@ -10,8 +10,17 @@ import Observation
 
 class FilmDetailViewModel {
     
-    // 반환할 사람(Person) 배열을 담는데 사용할 프로퍼티 선언
-    var people: [Person] = []
+    // Equatable은 동등성비교 프로토콜
+    enum State : Equatable  {
+        case idle
+        case loading
+        case loaded([Person])
+        case error(String)
+    }
+    
+    // state를 저장할 프로퍼티 선언 및 초기화
+    var state: State = State.idle
+    
     
     let service: GhibliService
     // DefaultGhibliService 를 기본값으로 사용하는 이니셜라이저
@@ -21,8 +30,13 @@ class FilmDetailViewModel {
     
     
     func fetch(for film: Film) async {
+        // state가 loading 상태가 아니면 중복호출 방지를 위해 return
+        guard state != State.loading else { return }
+        // loading상태를 확인했으니 state를 loading상태로 변경
+        state = .loading
         
-        
+        //fetch에서 병렬로 가져온 여러 Person데이터를 담을 빈 배열 초기화
+        var loadedPeople: [Person] = []
         
         do {
             //withThrowingTaskGroup 구조는 Task생성과 결과수집이 명시적으로 구분됨
@@ -31,6 +45,7 @@ class FilmDetailViewModel {
             // 각 Person URL마다 네트워크 요청수행
             // withThrowingTaskGroup을 사용하여 병렬로 여러 작업을 수행합니다.
             // ThrowingTaskGroup은 작업 중 하나라도 오류가 발생하면 전체 그룹이 실패하도록 합니다.
+            // 현재 병렬로 처리되고 있기 때문에 순서보장이 안되는 상태
             try await  withThrowingTaskGroup(of: Person.self){ group in
                 
                 for personInfoURL in film.people {
@@ -44,12 +59,21 @@ class FilmDetailViewModel {
                 // TaskGroup안의 Task가 끝나는 순서대로 결과를 하나씩 반환하고
                 // 잔환한 결과를 self.people 배열에 추가합니다.
                 for try await person in group {
-                    self.people.append(person)
+                    loadedPeople.append(person)
                 }
                 
             }
-        }catch {
             
+            // 모든 Person 데이터를 성공적으로 가져왔으므로 상태를 loaded로 변경하고
+            // 가져온 loadedPeople 배열을 상태에 담음
+            state = .loaded(loadedPeople)
+        }catch let error as APIError {
+            // APIError 타입의 오류가 발생하면
+            // errorDescription을 사용하여 오류 메시지를 상태에 담음
+            self.state = State.error(error.errorDescription ?? "Unknown error")
+        }catch {
+            // 오류발생시는 error상태로 저장
+            self.state = State.error("Unknown error")
         }
         
     }
@@ -59,15 +83,27 @@ class FilmDetailViewModel {
 import Playgrounds
 
 #Playground {
-    let viewModel = FilmDetailViewModel()
+    let service = MockGhibliService()
+    
+    let viewModel = FilmDetailViewModel(service: service)
     
     // Mock테스트용 서비스사용
-    let film = MockGhibliService().fetchPreviewFilm()
+    let film = service.fetchPreviewFilm()
     
     await viewModel.fetch(for: film)
-    
-    
-    for person in viewModel.people {
-        print(person)
+    // 이 switch문은 viewModel의 상태에 따라 다른 동작을 수행합니다.
+    // loaded 상태에서는 가져온 사람들의 정보를 출력합니다.
+    switch viewModel.state {
+    case .idle:
+        print("idle state")
+    case .loading:
+        print("loading state")
+        // 여기서의 let people는 Enum State의 연관값을 언래핑하는 역할
+    case .loaded(let people):
+         for person in people {
+            print(person)
+        }
+    case .error(let error):
+        print(error)
     }
 }
